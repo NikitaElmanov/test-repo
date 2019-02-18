3ebe3675380c9c27e3b4e9407e61ed7c2b760876
NikitaElmanov <nelmanov8@gmail.com>
2019-02-18 10:28:28 +0300

    add first java file in this project

--- TestGit/src/main/java/ru/test/git/entitties/BaseEntity.java
+++ TestGit/src/main/java/ru/test/git/entitties/BaseEntity.java
@@ -1,4 +1,21 @@
 package ru.test.git.entitties;
 
 public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
 }
