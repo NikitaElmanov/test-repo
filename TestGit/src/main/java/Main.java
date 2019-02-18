9df1df41de12a1815823919acfa2dda634ee8b58
NikitaElmanov <nelmanov8@gmail.com>
2019-02-18 10:55:37 +0300

    changed the main method

--- TestGit/src/main/java/Main.java
+++ TestGit/src/main/java/Main.java
@@ -1,12 +1,21 @@
+import ru.test.git.entitties.Person;
 import ru.test.git.service.PersonService;
+
+import java.util.Collection;
 
 public class Main {
 
     public static void main(String[] args) {
         PersonService personService = new PersonService();
 
+        System.out.println("call show method");
         personService.showAllPersons();
 
+        System.out.println("work with list of Persons in circle");
+        Collection list = personService.getAllPersons();
+        for (Object p : list){
+            System.out.println(((Person)p).getName());
+        }
 
     }
 
