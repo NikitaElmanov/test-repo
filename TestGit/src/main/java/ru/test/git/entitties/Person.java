2627221810e7c280295fd7ab9aa739f35a2cce36
NikitaElmanov <nelmanov8@gmail.com>
2019-02-18 10:40:36 +0300

    add person entity

--- TestGit/src/main/java/ru/test/git/entitties/Person.java
+++ TestGit/src/main/java/ru/test/git/entitties/Person.java
@@ -1,4 +1,14 @@
 package ru.test.git.entitties;
 
 public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
