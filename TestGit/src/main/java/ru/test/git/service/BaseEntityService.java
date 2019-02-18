f641a1db9c4eec4faced9cc4be596f1bd7a92d10
NikitaElmanov <nelmanov8@gmail.com>
2019-02-18 11:03:25 +0300

    added some inside filling for base entity

--- TestGit/src/main/java/ru/test/git/service/BaseEntityService.java
+++ TestGit/src/main/java/ru/test/git/service/BaseEntityService.java
@@ -1,4 +1,28 @@
 package ru.test.git.service;
 
+import ru.test.git.entitties.BaseEntity;
+import ru.test.git.entitties.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BaseEntityService {
+
+    List<BaseEntity> list = new ArrayList<BaseEntity>();
+
+    public BaseEntityService() {
+        this.list.addAll(Arrays.<BaseEntity>asList(new BaseEntity((long) 1), new BaseEntity((long) 2), new BaseEntity((long) 3)));
+    }
+
+    public List<BaseEntity> getAllPersons(){
+        return list;
+    }
+
+    public void showAllPersons(){
+        for (BaseEntity p : list){
+            System.out.println(p.getId());
+        }
+    }
+
 }
