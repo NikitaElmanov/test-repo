8ad342ce03273c3a88682fd9a4b05ade124c00ae
NikitaElmanov <nelmanov8@gmail.com>
2019-02-18 10:51:14 +0300

    add some service and the main method

--- TestGit/src/main/java/Main.java
+++ TestGit/src/main/java/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import ru.test.git.service.PersonService;
 
 public class Main {
+
+    public static void main(String[] args) {
+        PersonService personService = new PersonService();
+
+        personService.showAllPersons();
+
+
+    }
+
 }
--- TestGit/src/main/java/ru/test/git/service/PersonService.java
+++ TestGit/src/main/java/ru/test/git/service/PersonService.java
@@ -1,4 +1,27 @@
 package ru.test.git.service;
 
+import ru.test.git.entitties.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PersonService {
+
+    List<Person> list = new ArrayList<Person>();
+
+    public PersonService() {
+        this.list.addAll(Arrays.<Person>asList(new Person("Ann"), new Person("Tom"), new Person("Bob")));
+    }
+
+    public List<Person> getAllPersons(){
+        return list;
+    }
+
+    public void showAllPersons(){
+        for (Person p : list){
+            System.out.println(p.getName());
+        }
+    }
+
 }
